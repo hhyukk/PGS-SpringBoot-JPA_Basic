2c85ad15ec4111a2980ed71d08f7fbf8493b0c66
hhyukk <hhyukk1273@gmail.com>
2025-11-28 11:12:28 +0900

    feat: Post 엔티티에 생성일/수정일 필드 추가
    - createDate, modifyDate 추가로 게시글 생성 시각 기록 가능
    - 생성자에서 LocalDateTime.now() 로 기본 시간 설정

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -3,6 +3,8 @@ package com.back.domain.post.post.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity // 아래 구조대로 DB 테이블을 만들어야 한다.
@@ -14,11 +16,15 @@ public class Post {
     @Id
     @GeneratedValue(strategy = IDENTITY) // AUTO_INCREMENT
     private int id; // INT
+    private LocalDateTime createDate;
+    private LocalDateTime modifyDate;
     private String title; // VARCHAR(255)
     @Column(columnDefinition = "TEXT")
     private String content;
 
     public Post(String title, String content) {
+        this.createDate = LocalDateTime.now();
+        this.modifyDate = this.createDate;
         this.title = "";
         this.content = "";
     }
