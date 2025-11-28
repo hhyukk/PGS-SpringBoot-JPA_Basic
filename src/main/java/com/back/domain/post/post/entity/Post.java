80edd38beb046aa62a1adb22fa49cfe112e492f8
hhyukk <hhyukk1273@gmail.com>
2025-11-28 09:48:51 +0900

    refactor: Post 엔티티를 기본 생성자 기반으로 변경
    - @NoArgsConstructor 적용하여 JPA 기본 생성자 사용
    - 생성자 유지하되 필드 초기값 빈 문자열로 처리

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -1,27 +1,24 @@
 package com.back.domain.post.post.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity // 아래 구조대로 DB 테이블을 만들어야 한다.
 @Getter
 @Setter
-@RequiredArgsConstructor
 @ToString
+@NoArgsConstructor
 public class Post {
     @Id
     @GeneratedValue(strategy = IDENTITY) // AUTO_INCREMENT
     private int id; // INT
-    private final String title; // VARCHAR(255)
+    private String title; // VARCHAR(255)
     @Column(columnDefinition = "TEXT")
-    private final String content;
+    private String content;
 
-    public Post() {
+    public Post(String title, String content) {
         this.title = "";
         this.content = "";
     }
